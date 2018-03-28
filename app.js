164aea5477b5dd4f0051b95f57de1bf2d4adc761
KOKO0310 <mengyuangreat@163.com>
2018-03-28 13:09:47 +0800

    提取路由模块

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
+const router = require('./router');
+
 const app = express();
 
-app.get('/',function (req,res) {
-    res.send('hello world');
-});
+app.use(router);
 
 app.listen(5000,function () {
     console.log('5000 running....')
